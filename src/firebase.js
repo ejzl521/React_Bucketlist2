4cbae4a33a0fe551c54fa1e5b533f233f7183e59
이덕희 <qkwl521@naver.com>
2022-03-13 13:20:11 +0900

    Fix Redux

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import firebase from "firebase/compat/app"
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBB-NZ_Hd-Y8LCgdeJZdiUzqstqSU_6W78",
-    authDomain: "duckgugong-react.firebaseapp.com",
-    projectId: "duckgugong-react",
-    storageBucket: "duckgugong-react.appspot.com",
-    messagingSenderId: "455138718008",
-    appId: "1:455138718008:web:0e234daddc7a1cf4ac91cd",
-    measurementId: "G-3DSJ5X279T"
+    apiKey: "AIzaSyC6hC9Rpa0ZuONMHhjK89iKWouRDj7SXOA",
+    authDomain: "bucketlist-9f03f.firebaseapp.com",
+    projectId: "bucketlist-9f03f",
+    storageBucket: "bucketlist-9f03f.appspot.com",
+    messagingSenderId: "997397908951",
+    appId: "1:997397908951:web:4faf41010604286bfacf92",
+    measurementId: "G-0H7W5H3VFH"
 };
 
 // firebaseConfig 정보로 firebase 시작
